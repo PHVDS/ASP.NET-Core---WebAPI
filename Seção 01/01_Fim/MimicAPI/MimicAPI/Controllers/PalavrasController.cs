426e7db01aa9ed22046b24b2adc3ac8136608709
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-01-05 22:10:01 -0300

    PalavraController e API

---
+++ Seção 01/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimicAPI.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MimicAPI.Controllers
+{
+	public class PalavrasController : ControllerBase
+	{
+		private readonly MimicContext _banco;
+
+		public PalavrasController(MimicContext banco)
+		{
+			_banco = banco;
+		}
+
+		public ActionResult ObterPalavras()
+		{
+			//retorna um Json ou Xml
+			return Ok(_banco.Palavras);
+		}
+
+	}
+}

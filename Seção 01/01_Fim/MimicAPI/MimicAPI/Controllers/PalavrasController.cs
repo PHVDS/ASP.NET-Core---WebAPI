e70c475a76137b76c84881a566fd9c3ed2ce0b33
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-01-11 21:18:30 -0300

    Adicionando PalavraRepository

--- Seção 01/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
+++ Seção 01/01_Fim/MimicAPI/MimicAPI/Controllers/PalavrasController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MimicAPI.Database;
 using MimicAPI.Helpers;
 using MimicAPI.Models;
+using MimicAPI.Repositories.Contracts;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -14,11 +14,11 @@ namespace MimicAPI.Controllers
 	[Route("api/palavras")]
 	public class PalavrasController : ControllerBase
 	{
-		private readonly MimicContext _banco;
+		private readonly IPalavraRepository _repository;
 
-		public PalavrasController(MimicContext banco)
+		public PalavrasController(IPalavraRepository repository)
 		{
-			_banco = banco;
+			_repository = repository;
 		}
 
 		//APP -- /api/palavras	
@@ -26,32 +26,16 @@ namespace MimicAPI.Controllers
 		[HttpGet]
 		public ActionResult ObterTodas([FromQuery] PalavraUrlQuery query)
 		{
-			var item = _banco.Palavras.AsQueryable();
-			if(query.Data.HasValue)
+			var item = _repository.ObterPalavras(query);
+
+			if (query.PagNumero > item.Paginacao.TotalPaginas)
 			{
-				item = item.Where(a => a.Criado > query.Data.Value || a.Atualizado > query.Data.Value);
+				return NotFound();
 			}
 
-			//Paginacao
-			if (query.PagNumero.HasValue)
-			{
-				var quantidadeTotalRegistros = item.Count();
-				item = item.Skip((query.PagNumero.Value - 1) * query.PagRegistro.Value).Take(query.PagRegistro.Value);
+			Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(item.Paginacao));
 
-				var paginacao = new Paginacao();
-				paginacao.NumeroPagina = query.PagNumero.Value;
-				paginacao.RegistroPorPagina = query.PagRegistro.Value;
-				paginacao.TotalRegistros = quantidadeTotalRegistros;
-				paginacao.TotalPaginas = (int) Math.Ceiling((double) quantidadeTotalRegistros / query.PagRegistro.Value);
-
-				Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(paginacao));
-
-				if (query.PagNumero > paginacao.TotalPaginas)
-				{
-					return NotFound();
-				}
-			}
-			return Ok(item);
+			return Ok(item.ToList());
 		}
 
 		//WEB -- /api/palavras/1
@@ -59,12 +43,14 @@ namespace MimicAPI.Controllers
 		[HttpGet]
 		public ActionResult Obter(int id)
 		{
-			var obj = _banco.Palavras.Find(id);
+			var obj = _repository.Obter(id);
+
 			if (obj == null)
 			{
 				return NotFound();
 			}
-			return Ok();
+
+			return Ok(obj);
 		}
 
 		// -- /api/palavras(POST: id, nome, ativo, pontuacao, criacao)
@@ -72,8 +58,7 @@ namespace MimicAPI.Controllers
 		[HttpPost]
 		public ActionResult Cadastrar([FromBody] Palavra palavra)
 		{
-			_banco.Palavras.Add(palavra);
-			_banco.SaveChanges();
+			_repository.Cadastrar(palavra);
 
 			return Created($"//api/palavras/{palavra.Id}", palavra);
 		}
@@ -83,7 +68,7 @@ namespace MimicAPI.Controllers
 		[HttpPut]
 		public ActionResult Atualizar(int id, [FromBody] Palavra palavra)
 		{
-			var obj = _banco.Palavras.AsNoTracking().FirstOrDefault(a => a.Id == id);
+			var obj = _repository.Obter(id);
 
 			if (obj == null)
 			{
@@ -91,8 +76,8 @@ namespace MimicAPI.Controllers
 			}
 
 			palavra.Id = id;
-			_banco.Palavras.Update(palavra);
-			_banco.SaveChanges();
+			_repository.Atualizar(palavra);
+
 			return Ok();
 		}
 
@@ -101,15 +86,14 @@ namespace MimicAPI.Controllers
 		[HttpDelete]
 		public ActionResult Deletar(int id)
 		{
-			var palavra = _banco.Palavras.Find(id);
+			var palavra = _repository.Obter(id);
+
 			if (palavra == null)
 			{
 				return NotFound();
 			}
-
-			palavra.Ativo = false;
-			_banco.Palavras.Update(palavra);
-			_banco.SaveChanges();
+			_repository.Deletar(id);
+		
 			return NoContent();
 		}
 
--- Seção 01/01_Fim/MimicAPI/MimicAPI/Startup.cs
+++ Seção 01/01_Fim/MimicAPI/MimicAPI/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MimicAPI.Repositories;
+using MimicAPI.Repositories.Contracts;
+using AutoMapper;
 
 namespace MimicAPI
 {
@@ -17,11 +20,15 @@ namespace MimicAPI
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			
+
 			//configurando banco de dados
 			services.AddDbContext<MimicContext>( opt => {
 				opt.UseSqlite("Data Source=Database\\Mimic.db");
 			});
 			services.AddMvc();
+			services.AddScoped<IPalavraRepository, PalavraRepository>();
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

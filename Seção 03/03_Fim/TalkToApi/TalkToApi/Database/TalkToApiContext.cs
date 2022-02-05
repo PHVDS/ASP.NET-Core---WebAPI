25729c3cc1153b1053e6adcaa0c68c692955b49a
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-02-04 23:48:10 -0300

    UsuarioController - Cadastro e Login

--- Seção 03/03_Fim/TalkToApi/TalkToApi/Database/TalkToApiContext.cs
+++ Seção 03/03_Fim/TalkToApi/TalkToApi/Database/TalkToApiContext.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TalkToApi.V1.Models;
 
 namespace TalkToApi.Database
@@ -16,5 +12,6 @@ namespace TalkToApi.Database
 		}
 
 		public DbSet<Mensagem> Mensagem { get; set; }
+		public DbSet<Token> Tokens { get; set; }
 	}
 }
---
+++ Seção 03/03_Fim/TalkToApi/TalkToApi/V1/Repositories/Contracts/ITokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using TalkToApi.V1.Models;
+
+namespace TalkToApi.V1.Repositories.Contracts
+{
+	public interface ITokenRepository
+	{
+		void Cadastrar(Token token);
+		Token Obter(string refreshToken);
+		void Atualizar(Token token);
+	}
+}

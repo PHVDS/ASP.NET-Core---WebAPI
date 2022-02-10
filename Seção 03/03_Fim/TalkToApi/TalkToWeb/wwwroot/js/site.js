4efe17da0864fd37df246e15b82d88e572abc326
Paulo Henrique Vieira <30737597+PHVDS@users.noreply.github.com>
2022-02-10 16:22:48 -0300

    Criando Projeto Web(TalkToWeb) - Parte 1

---
+++ Seção 03/03_Fim/TalkToApi/TalkToWeb/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿function TesteCors() {
+    var tokenJWT = "";
+    var servico = "https://localhost:44327/api/mensagem/17d92682-ab6e-4c43-b9fe-4618acc8ea6f/42a26526-16df-4a39-88d2-ecc36b746bea";
+    $("#resultado").html("---Solicitando---");
+
+    $.ajax({
+        url: servico,
+        method: "GET",
+        crossDomain: true,
+        headers: { "Accept": "application/json" },
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + tokenJWT);
+        },
+        success: function (data, status, xhr) {
+            $("#resultado").html(data);
+        }
+    });
+}
